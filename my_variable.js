cc537b75fdf90221fbe5dce5d947dde7eac48808
Thabiso22 <thabisobudaza@gmail.com>
2016-11-03 12:30:49 +0200

    JS

---
+++ my_variable.js
@@ -0,0 +1,15 @@
+var sunshine = true;
+var age = 40;
+var surname = "Mrhwetyana";
+console.log(sunshine);
+console.log(typeof sunshine);
+console.log(age);
+console.log(typeof age);
+console.log(surname);
+console.log(typeof surname);
+var date = new Date();
+var fruit = ["apple","banana"];
+console.log(date);
+console.log(typeof date);
+console.log(fruit);
+console.log(typeof fruit);
